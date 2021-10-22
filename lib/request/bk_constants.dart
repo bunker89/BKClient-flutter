d6c4a8146df817aacc20623ed8095648e793c991
bunker89 <bunker.ys89@gmail.com>
2021-10-22 12:06:03 +0900

    Update bk_constants.dart

--- lib/request/bk_constants.dart
+++ lib/request/bk_constants.dart
@@ -3,6 +3,7 @@ class BKConstants {
   static const String BK_WORKING = "bk-working";
   static const String BK_WORKING_RESULT = "bk-result";
 
+  static const String LIMIT_OFFSET = "limit_base";
   static const String LIMIT_OFFSET = "limit_offset";
   static const String LIMIT_RANGE = "limit_range";
   static const String LIMIT_ARRAY = "limit_array";
